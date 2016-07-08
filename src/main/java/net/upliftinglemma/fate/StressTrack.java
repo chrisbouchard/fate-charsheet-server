a12a84d4743a53ab65997359b8ea3299605ab058
Chris Bouchard <cbouchard@automate.com>
2016-07-08 16:57:59 -0400

    Make StressTrack an entity.
    This way it can embed its collection of stress boxes.

--- src/main/java/net/upliftinglemma/fate/StressTrack.java
+++ src/main/java/net/upliftinglemma/fate/StressTrack.java
@@ -1,23 +1,32 @@
 package net.upliftinglemma.fate;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.persistence.Basic;
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 
-@Embeddable
+@Entity
 @Data
 public class StressTrack implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Basic(optional = false)
+    @SequenceGenerator(name = "character_seq", sequenceName = "character_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "character_seq")
+    @Id private Integer id;
+
     @NotNull private String name;
-    
-    private int max;
+
+    @ElementCollection
+    @Column(name = "value")
+    @OrderColumn(name = "index")
+    private List<Boolean> boxes;
+
+    @ManyToOne
+    private Character character;
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,17 @@ insert into character_aspects (character_id, index, type, name) values (1, 2, NU
 insert into character_skills (character_id, name, rank) values (1, 'Foo', 1);
 insert into character_skills (character_id, name, rank) values (1, 'Bar', 1);
 insert into character_skills (character_id, name, rank) values (1, 'Baz', 2);
+
+INSERT INTO stress_track (id, character_id, index, name) VALUES (1, 1, 0, 'Physical');
+INSERT INTO stress_track (id, character_id, index, name) VALUES (2, 1, 1, 'Mental');
+
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (1, 0, TRUE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (1, 1, FALSE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (1, 2, TRUE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (1, 3, FALSE);
+
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (2, 0, FALSE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (2, 1, FALSE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (2, 2, FALSE);
+INSERT INTO stress_track_boxes (stress_track_id, index, value) VALUES (2, 3, FALSE);
+
